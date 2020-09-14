1fae73801ae271bd5facbc4466e37b15cd8127ec
stryder03 <stryder03@users.noreply.github.com>
2020-09-14 14:55:58 -0600

    fix(form): removed date regex pattern

--- src/pages/memberships.tsx
+++ src/pages/memberships.tsx
@@ -68,7 +68,7 @@ const useStyles =makeStyles((theme) => ({
         }
     }
 }))
-const datePattern = RegExp("(((0[13578]|1[02])\\/(0[1-9]|1[0-9]|2[0-9]|3[01])|(0[469]|11)(0[1-9]|1[0-9]|2[0-9]|30)|(02)(0[1-9]|1[0-9]|2[0-8]))\\/(19|20)(\\d{2})|(0229)(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96))")
+// const datePattern = RegExp("(((0[13578]|1[02])\\/(0[1-9]|1[0-9]|2[0-9]|3[01])|(0[469]|11)(0[1-9]|1[0-9]|2[0-9]|30)|(02)(0[1-9]|1[0-9]|2[0-8]))\\/(19|20)(\\d{2})|(0229)(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96))")
 const metaDescription= "Join the Marine Corps League Detachment and support our mission in the Gallatin Valley"
 
 const getSteps = (appType: string) => {
@@ -174,7 +174,7 @@ const getStepContent: (step: number, appType: string) => formFieldProps[] = (ste
                         type: "date",
                         id: name,
                         variant: "filled",
-                        inputRef: {pattern: datePattern},
+                        inputRef: {},
                         margin: "normal",
                         required: false,
                         autoComplete: "",
@@ -189,7 +189,7 @@ const getStepContent: (step: number, appType: string) => formFieldProps[] = (ste
                         type: "date",
                         id: name,
                         variant: "filled",
-                        inputRef: {pattern: datePattern},
+                        inputRef: {},
                         margin: "normal",
                         required: false,
                         autoComplete: "",
