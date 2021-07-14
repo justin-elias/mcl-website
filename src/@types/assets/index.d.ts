59bac72a29c882c215abeddb138ab51aa17f69c1
stryder03 <stryder03@users.noreply.github.com>
2021-07-14 14:56:11 -0600

    fix(types): Corrected image types

--- src/@types/assets/index.d.ts
+++ src/@types/assets/index.d.ts
@@ -6,16 +6,16 @@ declare module "\*.svg" {
 }
 
 declare module "\*.jpg" {
-    const content: string;
-    export default content;
+    const content: any;
+    export default content
 }
 
 declare module "\*.png" {
-    const content: string;
-    export default content;
+    const content: any;
+    export default content
 }
 
 declare module "\*.json" {
-    const content: string;
-    export default content;
+    const content: any;
+    export default content
 }
